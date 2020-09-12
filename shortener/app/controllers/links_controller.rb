8e369354521f151338df1454e6faf34398193e89
federico <espinosa_816@hotmail.com>
2020-09-11 23:38:49 -0300

    views, methods in controller

--- shortener/app/controllers/links_controller.rb
+++ shortener/app/controllers/links_controller.rb
@@ -1,3 +1,16 @@
 class LinksController < ApplicationController
-	
+
+	def create
+		original_url = link_params[:original_url]
+		shortener = Shortener.new(original_url)
+		@link = shortener.generate_short_link
+	end
+
+	private	
+
+	def link_params
+
+		params.require(:link).permit(:original_url)
+	end
+
 end
--- shortener/app/models/link.rb
+++ shortener/app/models/link.rb
@@ -1,4 +1,12 @@
 class Link < ApplicationRecord
 	validates_uniqueness_of :short_url
+	validate :format_original_url
 	validates_presence_of :short_url, :original_url 
+
+	def format_original_url
+		uri = URI.parse(original_url || "")
+		if uri.host.nill?
+			errors.add(:original_url, "invalid URL format")
+		end		
+	end
 end
